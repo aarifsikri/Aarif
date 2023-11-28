37ec6d43c1f15e70e128dc9b7f8ec1b2b851772e
aarifsikri <144839475+aarifsikri@users.noreply.github.com>
2023-11-27 20:56:24 -0500

    commit-4
    pics added.

--- lab4/part-2/main.js
+++ lab4/part-2/main.js
@@ -5,8 +5,18 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const arrayofimage = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+
+
 
 /* Declaring the alternative text for each image file */
+const text= {
+    'pic1.jpg' : 'human eye',
+    'pic2.jpg' : 'Rocks',
+    'pic3.jpg' : 'Purple and white flowers',
+    'pic4.jpg' : 'painting',
+    'pic5.jpg' : 'a leaf'
+  }
 
 /* Looping through images */
 
