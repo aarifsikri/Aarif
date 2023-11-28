0d1181d62743747a561d123c43f1f1dbd5956a66
aarifsikri <144839475+aarifsikri@users.noreply.github.com>
2023-11-27 20:13:33 -0500

    commit-3
    text created

--- lab4/part-1/raw-text.js
+++ lab4/part-1/raw-text.js
@@ -1,5 +1,3 @@
-1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
-
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
@@ -9,23 +7,15 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-2. RAW TEXT STRINGS
 
-It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 
-Willy the Goblin
-Big Daddy
-Father Christmas
 
-the soup kitchen
-Disneyland
-the White House
+const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+const insertY =  ['the soup kitchen', 'Disneyland','the White House'];
+const insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
-spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away
 
-3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 randomize.addEventListener('click', result);
 
